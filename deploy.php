cfe1efe8d24dd4a4a775e503052dcb3c69c0c266
najoukou8 <l.najla1999@hotmail.fr>
2024-07-05 10:04:45 +0200

    f

--- deploy.php
+++ deploy.php
@@ -15,30 +15,55 @@ set('repository', 'https://github.com/najoukou8/StudentApp.git');
 set('git_tty', true); 
 
 // Shared files/dirs between deploys 
-add('shared_files', []);
+add('shared_files', ['.env']);
 add('shared_dirs', []);
 
 // Writable dirs by web server 
 add('writable_dirs', []);
 set('allow_anonymous_stats', false);
 
-set('param', 'value');
-
-task('deploy', function () {
-    $param = get('param');
-});
-
 // Hosts
 
 host('testHostdepd')
-    ->set('deploy_path', '~/{{application}}')
+    ->set('deploy_path', '~/{{application}}');
+    ->forwardAgent()
     ->port(25560);
      
 // Tasks
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:symlink',
+]);
 
 task('build', function () {
-   run('cd {{release_path}} && build');
+   run('cd {{release_path}}');
+   run('docker-composer build');
+   run('docker-compose up');
 });
+
+task('upload', function () {
+    upload(__DIR__ . "/", '{{release_path}}');
+});
+
+task('release', [
+    'deploy:prepare',
+    'deploy:release',
+    'upload',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:symlink',
+]);
+
+task('deploy', [
+    'build',
+    'release',
+    'cleanup',
+    'success'
+]);
 
 
 // [Optional] if deploy fails automatically unlock.
